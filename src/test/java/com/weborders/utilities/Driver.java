d4300795fb63b17eb65a82c1004ce7f076f1a9fa
nurialis <nurialis34@gmail.com>
2020-07-08 14:51:55 -0500

    Selenium

--- src/test/java/com/weborders/utilities/Driver.java
+++ src/test/java/com/weborders/utilities/Driver.java
@@ -1,6 +1,41 @@
 package com.weborders.utilities;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class Driver {
 
+    private static WebDriver driver;
 
+    private Driver(){
+
+    }
+    public WebDriver getDriver (){
+        String browser = ConfigurationReader.getProperty("browser");
+        if(driver == null){
+            switch(browser){
+                case "chrome":
+                case "Chrome":
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+                    break;
+                case "firefox":
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver();
+                    break;
+                default:
+                    throw new RuntimeException("Wrong browser name!");
+        }
+        }
+        return driver;
+    }
+    public static void closeDriver(){
+         if(driver != null){
+            driver.quit();
+            driver = null;
+
+        }
+    }
 }
